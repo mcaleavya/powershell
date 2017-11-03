37619064a6b46af65963c8d147f371c20215bd62
Allan McAleavy <Allan.McAleavy@gmail.com>
2017-11-03 12:22:49 +0000

    Update netbw.ps1
    Do Each Ethernet Interface and record TCP connection counts, errors and discards

--- netbw.ps1
+++ netbw.ps1
@@ -1,7 +1,9 @@
 # Allan McAleavy network stats script.
-# October 27th 2017
-#
-"{0,-30}{1,-30}{2,-30}{3,-30}" -f "Date Time","Name","Read MB/sec","Write MB/sec"
+# Added in TCP counts.
+# set execute policy for my user - damb powerhell..
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+"{0,-30}{1,-20}{2,-20}{3,-20}{4,-12}{5,-10}{6,-10}{7,-10}{8,-10}{9,-10}{10,-10}{11,-11}{12,-10}{13,-11}" -f "Date Time","Name","Read MB/sec","Write MB/sec","Esablished ","Listen ","TimeWait ","CloseWait","Bound","Total","Out Err","Recv Err","Out Disc","In Disc"
 while($true)
 {
 
@@ -9,11 +11,45 @@ while($true)
 $Start = Get-NetAdapterStatistics -Name "Eth*"
 sleep -Seconds 1
 $Next=Get-NetAdapterStatistics -Name "Eth*"
-For( $i = 0 ; $i -lt $Start.Count; $i++)
+$conns=Get-NetTCPConnection
+$lsn=0;
+$est=0;
+$tw=0;
+$cw=0;
+$bnd=0;
+$oerr=0;
+$rerr=0;
+$odisc=0;
+$idisc=0;
+For( $i = 0 ; $i -lt $Start.Count; $i++) 
 {
-$interface = $Start[$i].Name
+$interface = $Start[$i].Name 
 [int]$interface_rxb = ($Next[$i].ReceivedBytes - $Start[$i].ReceivedBytes) / 1048576
 [int]$interface_txb = ($Next[$i].SentBytes - $Start[$i].SentBytes) / 1048576
-"{0,-30}{1,-30}{2,-30}{3,-30}" -f` (Get-Date),$interface , $interface_rxb , $interface_txb
+$oerr += $Start[$i].OutboundPacketErrors
+$rerr += $Start[$i].ReceivedPacketErrors
+$odisc += $Start[$i].OutboundDiscardedPackets
+$idisc += $Start[$i].ReceivedDiscardedPackets
 }
+
+For( $i = 0 ; $i -lt $conns.Count; $i++) 
+{
+if ( $conns[$i].State -Contains "Listen") { $lsn++;}
+if ( $conns[$i].State -Contains "Established") { $est++;}
+if ( $conns[$i].State -Contains "TimeWait") { $tw++;}
+if ( $conns[$i].State -Contains "CloseWait") { $cw++;}
+if ( $conns[$i].State -Contains "Bound") { $bnd++;}
+
+$tot=($est+$lsn+$tw+$cw+$bnd)
 }
+"{0,-30}{1,-20}{2,-20}{3,-20}{4,-12}{5,-10}{6,-10}{7,-10}{8,-10}{9,-10}{10,-10}{11,-11}{12,-10}{13,-11}" -f` (Get-Date),$interface , $interface_rxb , $interface_txb,$est,$lsn,$tw,$cw,$bnd,$tot,$oerr,$rerr,$odisc,$idisc
+$lsn=0;
+$est=0;
+$tw=0;
+$cw=0;
+$bnd=0;
+$oerr=0;
+$rerr=0;
+$odisc=0;
+$idisc=0;
+} 
